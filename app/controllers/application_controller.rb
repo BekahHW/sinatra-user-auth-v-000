e26d282b2fccdf77009e30caac99149a22c169bc
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-16 17:19:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
-    enable :sessions/login
+    enable :sessions
     set :session_secret, "secret"
   end
 
